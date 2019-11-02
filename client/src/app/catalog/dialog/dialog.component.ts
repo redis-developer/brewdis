621b657e92a8cf9c6989460a142e9906d01523bb
Julien Ruaux <jruaux@gmail.com>
2019-11-01 17:21:18 -0700

    reworked catalog page

---
+++ client/src/app/catalog/dialog/dialog.component.ts
@@ -0,0 +1,14 @@
+import { Component, Inject } from '@angular/core';
+import { MAT_DIALOG_DATA } from '@angular/material/dialog';
+
+@Component({
+  selector: 'app-dialog',
+  templateUrl: './dialog.component.html',
+  styleUrls: ['./dialog.component.css']
+})
+export class DialogComponent {
+
+  constructor(@Inject(MAT_DIALOG_DATA) public product: any) { }
+
+
+}
